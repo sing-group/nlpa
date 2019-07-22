c211a15cd31be1974684de5c1d56412e7c1a3db6
José Ramón Méndez Reboredo <moncho.mendez@gmail.com>
2019-07-22 11:06:18 +0200

    Fixed error on token sequence

--- src/main/java/org/nlpa/pipe/impl/TokenSequencePorterStemmerPipe.java
+++ src/main/java/org/nlpa/pipe/impl/TokenSequencePorterStemmerPipe.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -169,7 +169,7 @@ public class TokenSequencePorterStemmerPipe extends AbstractPipe {
      * Default constructor
      */
     public TokenSequencePorterStemmerPipe() {
-        super(new Class<?>[]{GuessLanguageFromStringBufferPipe.class}, new Class<?>[]{TokenSequencePorterStemmerPipe.class});
+        super(new Class<?>[]{GuessLanguageFromStringBufferPipe.class}, new Class<?>[]{TokenSequenceStemIrregularPipe.class});
         this.langProp = DEFAULT_LANG_PROPERTY;
     }
 
