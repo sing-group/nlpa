c48370d49616370a4d9cc8b8fbf6bdaac068ceb7
José Ramón Méndez Reboredo <moncho.mendez@gmail.com>
2020-01-16 13:36:55 +0100

    Some improvements of LeetSpeak recognising
    + Dictionary has been improbed
    + The processing is made word by word (not in the whole text) and only words with leet are processed.

--- src/main/java/org/nlpa/pipe/impl/LeetSpeakFromStringBuffer.java
+++ src/main/java/org/nlpa/pipe/impl/LeetSpeakFromStringBuffer.java
@@ -9,10 +9,18 @@ import org.bdp4j.pipe.TransformationPipe;
 import org.bdp4j.types.Instance;
 import org.nlpa.util.LeetSpeak;
 
+/**
+ * Leet Speak decoder for instances
+ * 
+ * @author José Ramón Méndez Reboredo
+ */
 @AutoService(Pipe.class)
 @TransformationPipe
 public class LeetSpeakFromStringBuffer extends AbstractPipe {
 
+    /**
+     * Default constructor for leet speak decoder pipe
+     */
     public LeetSpeakFromStringBuffer() {
         super(new Class<?>[0], new Class<?>[0]);
     }
@@ -29,7 +37,7 @@ public class LeetSpeakFromStringBuffer extends AbstractPipe {
 
     @Override
     public Instance pipe(Instance carrier) {
-        carrier.setData(new StringBuffer(LeetSpeak.decode(carrier.getData().toString())));
+        carrier.setData(LeetSpeak.decode((StringBuffer)carrier.getData()));
         return carrier;
     }
 
