678306bba0bed1a1e0e4f2d8e4ec72e4884ad75b
PatriciaMaPe <martinperez.patricia@gmail.com>
2020-04-13 14:44:42 +0200

    Create pipe to compute the compression ratio

---
+++ src/main/java/org/nlpa/pipe/impl/ComputeCompressionRatioPipe.java
@@ -0,0 +1,130 @@
+package org.nlpa.pipe.impl;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bdp4j.pipe.AbstractPipe;
+import org.bdp4j.pipe.PropertyComputingPipe;
+import org.bdp4j.types.Instance;
+import org.bdp4j.pipe.Pipe;
+import org.bdp4j.pipe.PipeParameter;
+
+import com.google.auto.service.AutoService;
+
+/**
+ * This pipe compute the compression ratio between the original text size and the compressed text size.
+ *
+ * @author Patricia Martin Perez
+ */
+@PropertyComputingPipe
+@AutoService(Pipe.class)
+public class ComputeCompressionRatioPipe extends AbstractPipe {
+
+    /**
+     * For logging purposes
+     */
+    private static final Logger logger = LogManager.getLogger(ComputeCompressionRatioPipe.class);
+
+    /**
+     * The default property name where the compression ratio will be stored
+     */
+    public static final String DEFAULT_COMPRESSION_RATIO_PROPERTY = "compressionRatio";
+
+    /**
+     * The name of the property where the compression ratio is stored
+     */
+    private String compressionRatioProperty = DEFAULT_COMPRESSION_RATIO_PROPERTY;
+
+    /**
+     * Construct a ComputeStringCompressionRatioPipe instance
+     *
+     */
+    public ComputeCompressionRatioPipe() {
+        super(new Class<?>[0], new Class<?>[0]);
+
+    }
+
+    /**
+     * Return the input type included the data attribute of an Instance
+     *
+     * @return the input type for the data attribute of the Instance processed
+     */
+    @Override
+    public Class<?> getInputType() {
+        return StringBuffer.class;
+    }
+
+    /**
+     * Indicates the datatype expected in the data attribute of an Instance
+     * after processing
+     *
+     * @return the datatype expected in the data attribute of an Instance after
+     * processing
+     */
+    @Override
+    public Class<?> getOutputType() {
+        return StringBuffer.class;
+    }
+
+    /**
+     * Establish the name of the property where the compression ratio will be stored
+     *
+     * @param compressionRatioProperty The name of the property for storing the compression ratio
+     */
+    @PipeParameter(name = "compressionRatioProperty", description = "Indicates the property name to store the compression ratio", defaultValue = DEFAULT_COMPRESSION_RATIO_PROPERTY)
+    public void setCompressionRatioProperty(String compressionRatioProperty) {
+        this.compressionRatioProperty = compressionRatioProperty;
+    }
+
+    /**
+     * Retrieves the property name for storing the compression ratio
+     *
+     * @return String containing the property name for storing the compression ratio
+     */
+    public String getCompressionRatioProperty() {
+        return this.compressionRatioProperty;
+    }
+
+    @Override
+    public Instance pipe(Instance carrier) {
+        if (carrier.getData() instanceof StringBuffer) {
+        	double ratio = 0;
+            String text = carrier.getData().toString();
+            int originalTextSize = text.getBytes().length;
+            int compressedTextSize = 0;
+            
+            try {
+            	
+            	ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+				GZIPOutputStream zipOutputStream = new GZIPOutputStream(byteOutputStream);
+				zipOutputStream.write(text.getBytes());
+
+				compressedTextSize = byteOutputStream.size();
+				
+				byteOutputStream.close();
+	            zipOutputStream.close();
+	            
+	            
+			} catch (IOException e) {
+				logger.error("Exception output streams " + e.getMessage());
+			}
+            
+            if(originalTextSize != 0) {
+            	 ratio = (double) compressedTextSize / (double) originalTextSize;
+            }
+            
+            double ratioDecimalFormat = (double) Math.round(ratio * 100) / 100;
+            carrier.setProperty(compressionRatioProperty, ratioDecimalFormat);
+            
+        } else {
+            logger.error("Data should be a StringBuffer when processing " + carrier.getName() + " but is a "
+                    + carrier.getData().getClass().getName());
+        }
+
+        return carrier;
+    }
+
+}
---
+++ src/test/java/org/nlpa/pipe/impl/ComputeCompressionRatioPipeTest.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.nlpa.pipe.impl;
+
+import org.bdp4j.types.Instance;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Patricia Martin Perez
+ */
+public class ComputeCompressionRatioPipeTest {
+
+	String data = "DECÉMBER is hre :), HO HO HO! 🎅 Beat the Christmas days with us and we'll even give you 19% off online until 31 DEC.";
+    String name = "basic_example/_spam_/7c63a8fd7ae52e350e354d63b23e1c3b.tsms";
+    String source = "basic_example/_spam_/7c63a8fd7ae52e350e354d63b23e1c3b.tsms";
+    private static Instance carrier = null;
+
+    private ComputeCompressionRatioPipe instance;
+
+    public ComputeCompressionRatioPipeTest() {
+    }
+
+    @Before
+    public void setUp() {
+        instance = new ComputeCompressionRatioPipe();
+        carrier = new Instance(new StringBuffer(data), null, name, source);
+    }
+
+    /**
+     * Test getInputType method of class ComputeStringCompressionRatioPipe.
+     */
+    @Test
+    public void testGetInputType() {
+        Class<?> expResult = StringBuffer.class;
+        Class<?> result = instance.getInputType();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test getOutputType method of class ComputeStringCompressionRatioPipe.
+     */
+    @Test
+    public void testGetOutputType() {
+        Class<?> expResult = StringBuffer.class;
+        Class<?> result = instance.getOutputType();
+        assertEquals(expResult, result);
+    }
+
+    
+    /**
+     * Test getCompressionRatioProperty method of class ComputeStringCompressionRatioPipe.
+     */
+    @Test
+    public void testGetCompressionRatioProp() {
+        String expResult = "compressionRatio";
+        String result = instance.getCompressionRatioProperty();
+        assertEquals(expResult, result);
+    }
+
+
+    /**
+     * Test setCompressionRatioProperty method of class ComputeStringCompressionRatioPipe.
+     */
+    @Test
+    public void testSetCompressionRatioProp() {
+        String compressionRatioProperty = "compressionRatio";
+        instance.setCompressionRatioProperty(compressionRatioProperty);
+    }
+
+
+    /**
+     * Test pipe method of class ComputeStringCompressionRatioPipe.
+     */
+    @Test
+    public void testPipe() {
+        Instance expResult = new Instance(new StringBuffer(data), null, name, source);
+        expResult.setProperty("compressionRatio", 0.08);
+
+        Instance result = instance.pipe(carrier);
+        assertTrue(expResult.equals(result));
+    }
+
+}
